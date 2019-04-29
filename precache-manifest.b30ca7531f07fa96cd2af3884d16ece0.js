98a237bf184c576521f0baa0426a9b96de600360
AlenaNik <alena.nik.digital@gmail.com>
2019-04-29 09:46:13 +0200

    Updates

---
+++ precache-manifest.b30ca7531f07fa96cd2af3884d16ece0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d40000055bff7685c5c9",
+    "url": "/React5/static/css/main.af16981c.chunk.css"
+  },
+  {
+    "revision": "d40000055bff7685c5c9",
+    "url": "/React5/static/js/main.996f46d8.chunk.js"
+  },
+  {
+    "revision": "adbfbc226d5abe5df745",
+    "url": "/React5/static/js/runtime~main.c697e53e.js"
+  },
+  {
+    "revision": "04346ad3abeb3f8f1852",
+    "url": "/React5/static/css/2.70f3608c.chunk.css"
+  },
+  {
+    "revision": "04346ad3abeb3f8f1852",
+    "url": "/React5/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "598469e7ec9f9b40684dc53ddd0d06a4",
+    "url": "/React5/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -11,19 +11,13 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.700468aadae2b2b74ed2268258723fbf.js"
+  "/React5/precache-manifest.b30ca7531f07fa96cd2af3884d16ece0.js"
 );
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
-
-workbox.core.clientsClaim();
+workbox.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -31,9 +25,10 @@ workbox.core.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute("/React5/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
